404694ee975cfda253d4ca28cc5184dfc29247f3
Hermannovich <donfack.hermann@gmail.com>
2019-04-10 03:35:56 +0200

    Change twitter title link

--- src/components/bio.js
+++ src/components/bio.js
@@ -40,7 +40,7 @@ function Bio() {
             <p>
               Written by <strong>{author}</strong>.{` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                follow us on Twitter
               </a>
             </p>
           </div>
